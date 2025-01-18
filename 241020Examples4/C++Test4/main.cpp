8541c5718b8211c313f428d42d0c77dacc337396
Shtekata <gesheval@gmail.com>
2025-01-18 19:41:03 +0200

    241020Examples4

---
+++ 241020Examples4/C++Test4/main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	char a, b;
+	char A, B;
+	char symbol;
+	char Symbol;
+	char c = 'A';
+	cout << (int)'a' << endl;
+	cout << (char)97 << endl;
+	cout << (char)(20 + 45) << endl;
+}
