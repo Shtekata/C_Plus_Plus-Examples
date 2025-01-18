6782d867e8166b237f2413af2744155989b25bba
Shtekata <gesheval@gmail.com>
2025-01-18 19:52:40 +0200

    241020Examples6

---
+++ 241020Examples6/C++Test6/main.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	/*int a, b, c;
+	a = 12;
+	b = a++;
+	c = ++b;
+	cout << "a = " << a << endl;
+	cout << "b = " << b << endl;
+	cout << "c = " << c << "\n\n";
+
+	int k = 20, p = 18;
+	double s = 8.0, r = 3.0;
+	cout << (k += p) << endl;
+	cout << (k /= p) << endl;
+	cout << (k %= p) << endl;
+	cout << (s *= r) << "\n\n";
+
+	cout << (24 && 0) << endl;
+	cout << (24 || 0) << endl;
+	cout << ('\0' && 2.5) << endl;
+	cout << ('\0' || 2.5) << "\n\n";
+
+	cout << ('5' * 2) << endl;
+	cout << (2 - 'A') * 2.1 << "\n\n";*/
+
+	double a = 5;
+	cout << a << endl;
+	int b = 3.52;
+	cout << b << "\n\n";
+
+	cout << (int)3.8 << endl;
+	cout << (char)42 << endl;
+	cout << (double)10 / 4 << endl;
+	cout << (double)(10 / 4) << endl;
+
+	return 0;
+}
